fbeae4ef5290f19ac45693e5a967374eafe2c2aa
priya8 <panditpriyanka93@gmail.com>
2014-05-09 08:23:44 +0530

    Create 3.php
    This 3.php contains code of displaying few more songs randomly from database since we have made 7 as our minimum songs to be selected for getting better results.

---
+++ 3.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+</head>
+<body>
+<form name="songs" action="4.php" method="POST" >
+<p><input type="checkbox" name="chckbox[]" value="raabta" />raabta</input></p>
+<p><input type="checkbox" name="chckbox[]" value="chikni chameli"/>chikni chameli</input></p>
+<p><input type="checkbox" name="chckbox[]" value="deewana"/>deewana</input></p>
+<p><input type="checkbox" name="chckbox[]" value="kal ho na ho"/>kal ho na ho</input></p>
+<p><input type="checkbox" name="chckbox[]" value="dhoom"/>dhoom</input></p>
+<p><input type="checkbox" name=”chckbox[]" value="sur"/>sur</input></p>
+<p><input type="checkbox" name="chckbox[]" value="sajde"/>sajde</input></p>
+<p><input type="checkbox" name="chckbox[]" value="guzzarish"/>guzzarish</input></p>
+<p><input type="checkbox" name="chckbox[]" value="earth song"/>earth song</input></p>
+<p><input type="checkbox" name="chckbox[]" value="bille jean"/>bille jean</input></p>
+<p> <input type="submit" name="submit" value="submit"/></p>
+</form>
+</body>
+</html>
+
+
